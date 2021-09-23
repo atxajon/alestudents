9e4dfa709b6a7caf4d0b4d6a1ab8821a57974770
Jon Atxa <atxajon@gmail.com>
2021-09-23 18:02:39 +0200

    improves styles of students table

--- resources/js/Pages/StudentsList.vue
+++ resources/js/Pages/StudentsList.vue
@@ -11,8 +11,8 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                   <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <table class="table-auto">
-                      <thead>
+                    <table class="divide-y divide-gray-300">
+                      <thead class="bg-gray-50">
                         <tr>
                           <th>
                             Name
@@ -21,30 +21,38 @@
                             Surname
                           </th>                          
                           <th>
-                            Action
+                            Edit
+                          </th>                          
+                          <th>
+                            Delete
                           </th>
 	                      </tr>
                       </thead>
-                      <tbody>
-                        <tr v-for="student in students" :key="student.id">
-                          <td>
+                      <tbody class="bg-white divide-y divide-gray-300">
+                        <tr v-for="student in students" :key="student.id" class="whitespace-nowrap">
+                          <td class="px-6 py-4 text-sm text-gray-500">
                             {{ student.name }}
                           </td>
-                          <td>
+                          <td class="px-6 py-4 text-sm text-gray-500">
                             {{ student.surname }}
                           </td>
-                          <td>
-                            <inertia-link :href="route('students.edit', student.id)">
-                              Edit
-                            </inertia-link>
-                          </td>
-                          <td>
-                            <inertia-link
-                              method="delete"
-                              :href="route('students.destroy', student.id)">
-                              Delete
-                            </inertia-link>
-                          </td>
+                          <td class="px-6 py-4">
+                                <jet-nav-link :href="route('students.edit', student.id)" as="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-blue-400" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                    </svg>
+                                </jet-nav-link>
+                            </td>
+                            <td class="px-6 py-4">
+                                <jet-nav-link method="delete" :href="route('students.destroy', student.id)" as="button">
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                    </svg>
+                                </jet-nav-link>
+                            </td>
                         </tr>
                       </tbody>
                     </table>
@@ -55,8 +63,10 @@
     </app-layout>
 </template>
 
+
 <script>
     import AppLayout from '@/Layouts/AppLayout.vue'
+    import JetNavLink from '@/Jetstream/NavLink.vue'
 
     export default {
         props: {
@@ -64,6 +74,7 @@
         },
         components: {
             AppLayout,
+            JetNavLink,
         },
     }
 </script>
